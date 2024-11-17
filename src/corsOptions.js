1c720df2aec924c63e842f9579a728c56d20c875
unknown <fearaujo871@gmail.com>
2024-11-16 23:09:34 -0300

    teste alteracao origin em corsOption.js

--- src/corsOptions.js
+++ src/corsOptions.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const corsOptions = {
-    origin: /*'http://localhost:3000'*/ process.env.CORS_ORIGIN,
+    origin: /*'http://localhost:3000'*/ 'https://api-consciencia-negra-k95eqfkpc-felipes-projects-af697b65.vercel.appciencia-negra.vercel.app',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELEYE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
